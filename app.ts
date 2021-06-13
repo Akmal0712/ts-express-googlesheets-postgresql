24db8cb8af20038ef6a5e227527e3acb1356d393
Akmal Safarov <undercover.103a@gmail.com>
2021-06-13 19:48:11 +0500

    cleaning

--- app.ts
+++ app.ts
@@ -1,18 +1,16 @@
 require('dotenv').config()
 import express, { Application } from 'express'
-const app: Application = express()
-
 import bodyParser from 'body-parser'
+import Router from './app/routes'
+
+const app: Application = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
-import Router from './app/routes'
 Router.start(app)
 
 const PORT = process.env.APP_PORT || 3000
-
 app.listen(PORT, () => {
     console.log(`Server working on port ${PORT}`)
 })
